e309340bcee7ef555ea6b70291e76accb480f4ee
JohannesG <johannes.girstmair@gmail.com>
2016-06-03 10:46:56 +0100

    Now added ATCG counter

--- dnacalc.py
+++ dnacalc.py
@@ -2,10 +2,24 @@
 
 # This is a comment
 
-DNASeq = 'ATGAAC'
+DNASeq = 'ATGAACC'
 
 print( 'Sequence ' + DNASeq )
 
-SeqLength = len( DNASeq )
+SeqLength = float( len( DNASeq ) )
+
+# to combine a string eg 'length is' with non-string things will break the code. Therefore I used str()
 
 print ( 'Length is ' + str( SeqLength ) )
+
+# .count means it will just count. in our case A
+
+NumberA = DNASeq.count('A')
+NumberT = DNASeq.count('T')
+NumberG = DNASeq.count('G')
+NumberC = DNASeq.count('C')
+
+print('A: ' + str( NumberA / SeqLength ) )
+print('T: ' + str( NumberT / SeqLength ) )
+print('G: ' + str( NumberG / SeqLength ) )
+print('C: ' + str( NumberC / SeqLength ) )
